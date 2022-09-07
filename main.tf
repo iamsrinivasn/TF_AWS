e01ea73b3c76124eb81f66059d9ea02feef96e92
Srinivas Niddapu <srini@axiomio.com>
2022-09-07 16:07:39 +0530

    cred migrated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
     region  = "us-west-2"
-    shared_credentials_file = ".aws/credentials"
+    shared_credentials_file = "~/.aws/credentials"
     profile = "tfadmin"
 }
 
