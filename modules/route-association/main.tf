c94fd0fb53e3edcbfe6219614fbd95b23389c1d7
Srinivas Niddapu <srini@axiomio.com>
2022-09-08 10:26:22 +0530

    update

---
+++ modules/route-association/main.tf
@@ -0,0 +1,15 @@
+resource "aws_route_table_association" "ROUTE_ASSOCIATION" {
+  for_each = tomap(var.SUBNET_CIDR)
+  cidr_block = each.value
+
+  subnet_id      = var.SUBNET_ID
+  route_table_id = var.ROUTE_ID
+}
+
+resource "aws_route_table_association" "ROUTE_ASSOCIATION" {
+  for_each = tomap(var.SUBNET_CIDR)
+  cidr_block = each.key
+
+  subnet_id      = var.SUBNET_ID
+  route_table_id = var.ROUTE_ID
+}
